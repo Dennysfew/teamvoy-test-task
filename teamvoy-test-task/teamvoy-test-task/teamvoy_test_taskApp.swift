860f163ec59d00140fda319da5f80480fa83b0e8
Dennys Izhyck <denysizhuk@gmail.com>
2023-08-05 21:01:43 +0300

    initial commit

---
+++ teamvoy-test-task/teamvoy-test-task/teamvoy_test_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  teamvoy_test_taskApp.swift
+//  teamvoy-test-task
+//
+//  Created by Denys on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct teamvoy_test_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
